f1a1ca6bc9c66decd75360253c3cd0bc89071c63
kosuke <kosuke.n27@gmail.com>
2014-10-13 03:27:41 +0900

    use with-open and eval lazyseq

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject random-user "0.1.0-SNAPSHOT"
   :main random-user.core
-  :description "FIXME: write description"
+  :description "get user information at random"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [http.async.client "0.5.2"]
-                 [org.clojure/data.json "0.2.5"]])
+                 [org.clojure/data.json "0.2.5"]
+                 [http.async.client "0.5.2"]])
--- src/random_user/core.clj
+++ src/random_user/core.clj
@@ -2,28 +2,36 @@
   (:require [http.async.client :as http]
             [clojure.data.json :as json]))
 
-(defn get-random-dummy-users-json
-  "get multi user vector"
+(def api-endpoint "http://api.randomuser.me/")
+
+(defn request
+  "get method wrapper"
+  [client]
+  (http/GET client api-endpoint))
+
+(defn get-random-dummy-users
+  "get multi users as vector"
   [n]
-  (let [url "http://api.randomuser.me/"
-        client (http/create-client)]
-    (map #(-> % http/await http/string)
-         (for [i (range n)] (http/GET client url)))))
+  (with-open [client (http/create-client)]
+    (let [resps (keep (fn [resp]
+                        (http/await resp)
+                        (if-not (http/failed? resp)
+                          (-> resp http/string json/read-str)))
+                      (for [_ (range n)] (request client)))]
+      (vec (doall resps)))))
 
 (defn get-random-dummy-user-json
-  "get user from http://randomuser.me/"
+  "get user from http://randomuser.me/ as json"
   []
   (with-open [client (http/create-client)]
-    (let [resp (http/GET client "http://api.randomuser.me/" :timeout 1000)]
+    (let [resp (request client)]
       (http/await resp)
-      (if (http/failed? resp)
-        (println "error")
+      (if-not (http/failed? resp)
         (http/string resp)))))
 
 (defn get-random-dummy-user
   "get user from http://randomuser.me/"
   []
   (let [resp (get-random-dummy-user-json)]
-    (if (= nil resp)
-      nil
+    (if-not (nil? resp)
       (json/read-str resp))))
